195246bf9d46533f4c61ee305128a0b385e0a3da
Irfan Akbari Habibi <irfanakbarihabibi18@gmail.com>
2023-12-07 14:23:23 +0700

    cloud build

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ app = Flask(__name__)
 register_blueprints(app)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=int(os.environ.get("PORT", 8000)))
+    app.run(debug=False, port=int(os.environ.get("PORT", 8000)))
