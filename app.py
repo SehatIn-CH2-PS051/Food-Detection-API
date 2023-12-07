c38b7106aabfb325f36cf767dceab3e1cfad3ef0
Irfan Akbari Habibi <irfanakbarihabibi18@gmail.com>
2023-12-07 21:33:59 +0700

    fix port

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ app = Flask(__name__)
 register_blueprints(app)
 
 if __name__ == '__main__':
-    app.run(debug=False,host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=False, host='0.0.0.0', port=8080)
